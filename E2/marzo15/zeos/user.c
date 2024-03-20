ba001d7039a8af9dd4b8fcb58e266800ac960d7f
Marcmota <marc.olivegonza@gmail.com>
2024-03-20 12:40:04 +0100

    getpid funcional

--- E2/marzo15/zeos/user.c
+++ E2/marzo15/zeos/user.c
@@ -9,6 +9,9 @@ int __attribute__ ((__section__(".text.main")))
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
-
+  char * msg;
+  int pid = getpid();
+  //itoa(pid,msg);
+  //write(1,msg,strlen(msg));
   while(1) {}
 }
